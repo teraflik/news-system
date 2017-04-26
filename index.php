d06325335b7f832bc28645c97238a5a11cd392c2
Raghav Khandelwal <raghavthecyberdude@gmail.com>
2017-04-26 13:15:34 +0530

    Fetch news from database

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include "dbconn.php";
+require('includes/dbconn.php');
 $page = "home"; 
 
 if( !isset($_SESSION['username']) ){
@@ -18,18 +18,21 @@ if( !isset($_SESSION['username']) ){
 </head>
 
 <body>
-	<?php include("includes/navbar.php"); ?>	
+	<?php include("includes/navbar.php"); ?>    
 	<div class="container">
-		<?php include("message.php"); ?>
+		<?php include("includes/message.php"); ?>
 		<div class="jumbotron">
-			<div class="col-sm-8 mx-auto">
-				<h1>Navbar examples</h1>
-				<p>This example is a quick exercise to illustrate how the navbar and its contents work. Some navbars extend the width of the viewport, others are confined within a <code>.container</code>. For positioning of navbars, checkout the <a href="../navbar-top/">top</a> and <a href="../navbar-top-fixed/">fixed top</a> examples.</p>
-				<p>At the smallest breakpoint, the collapse plugin is used to hide the links and show a menu button to toggle the collapsed content.</p>
-				<p>
-				<a class="btn btn-primary" href="../../components/navbar/" role="button">View navbar docs &raquo;</a>
-				</p>
-			</div>
+		<?php
+		 $result = $link->query('SELECT `newsID`, `title`, `post`, `timestamp` FROM `news` ORDER BY `newsID` DESC') or die($link->error);
+		while($row = $result->fetch_assoc()){
+			echo '<div class="col-sm-8 mx-auto">';
+				echo '<h1><a href="view.php?id='.$row['newsID'].'">'.$row['title'].'</a></h1>';
+				echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['timestamp'])).'</p>';
+				echo '<p>'.$row['post'].'</p>';                
+				echo '<p><a class="btn btn-primary" role="button" href="viewpost.php?id='.$row['newsID'].'">Read More</a></p>';                
+			echo '</div>';
+		}
+		?>
 		</div>
 	</div>
 	<?php include("includes/scripts.html"); ?>
